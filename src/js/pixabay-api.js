fcf6e8f7374b065b7536654514dcb608464025da
Anna.K <flysnow@ukr.net>
2026-05-05 20:47:44 +0300

    JS-HW-11, api-key-refreshed, JavaScript

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '23963114-6d0d5d874ae460d9125bacd21';
+const API_KEY = '55675951-c5be5de1598e9491b9f323a37';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export function getImagesByQuery(query) {
